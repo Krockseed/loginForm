6b8b790a2b7a36eb66bab10f288797652eabd273
seed <seed@seeds-MacBook-Air.local>
2022-05-12 21:50:14 +0900

    front connect js

--- app/main.js
+++ app/main.js
@@ -6,8 +6,9 @@ const path = require('path');
 
 const home = require('./src/routes/home');
 
-app.use(express.static(path.join(__dirname + '../public')));
-app.set('views', path.join(__dirname, 'src/views/home'));
+app.use(express.static(`${__dirname}/src/public`));
+//app.use(express.static(path.join(__dirname + '../public')));
+app.set('views', `${__dirname}/src/views/home`);
 app.set('view engine', 'ejs');
 
 app.use('/', home);

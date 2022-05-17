203022f5469c3a4efbffe958cdfcb0fe5e53d3df
seed <seed@seeds-MacBook-Air.local>
2022-05-17 18:27:05 +0900

    addEventListener

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('hello');
+const id = document.querySelector('#id'),
+    paswrd = document.querySelector('#paswrd'),
+    loginBtn = document.querySelector('button');
+
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        password: paswrd.value
+    };
+
+    console.log(req);
+}
